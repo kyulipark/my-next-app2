f9085fa9ecb44f50edcb489dd013f393abe3e193
Kyuli <admin@adminui-iMac.local>
2024-09-26 21:47:53 +0900

    add: db.json 서버 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import ClientExample from "./component/ClientExample";
 // }
 
 export default function Home() {
-	console.log("안녕, 난 서버 컴포넌트야.")
+	console.log("안녕, 난 클라이언트 컴포넌트야.")
 
 	return <div className="p-8 m-4"> 안녕하세요! Next.js 입니다. 
 	<ClientExample />
